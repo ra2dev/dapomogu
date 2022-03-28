10dc0117f14d5fd5aff934ccccfddb22dfdbeccf
ra2dev <raramanouski@gmail.com>
2022-03-28 09:35:41 +0200

    Updated text.

--- components/questions/RootQuestion.tsx
+++ components/questions/RootQuestion.tsx
@@ -4,10 +4,13 @@ import { Text } from "@nextui-org/react";
 export const RootQuestion = () => {
   return (
     <div className="text-left pt-2 md:pt-7">
-      <Text weight="bold" className="text-[27px] md:text-[36px] leading-8 md:leading-10 mb-5">
+      <Text
+        weight="bold"
+        className="text-[27px] md:text-[36px] leading-8 md:leading-10 mb-5"
+      >
         Как спастись от <br />
-        <b className="text-[#DE3663]">«спецоперации в Украине»</b><br /> даже
-        если вы не в Украине. И как помочь пострадавшим от неё
+        <b className="text-[#DE3663]">российской военной агрессии</b>
+        <br /> даже если вы не в Украине. <br />И как помочь пострадавшим от неё
       </Text>
       <p className="text-md text-gray-600 mb-8 leading-6 hidden md:block">
         Простой опросник приведёт вас к ответу. Мы обновляем информацию, но
